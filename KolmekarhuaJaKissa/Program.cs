43cb955c5d7b79f4a7cf3f00a7afdc87124d5be1
s2401221 <s2401221@BC-PF51BGSQ>
2025-01-15 13:21:42 +0200

    uusi projekti kissa ja karhu

--- KolmekarhuaJaKissa/Program.cs
+++ KolmekarhuaJaKissa/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Kuinkas monta kalaa on karhut napannut???");
+            String kala = Console.ReadLine();
+            Console.WriteLine($"Kissalle menee");
         }
     }
 }
