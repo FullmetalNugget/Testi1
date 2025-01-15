7c0a0cdc278c1bc113b462256a077c04c7ca5e0b
s2401221 <s2401221@BC-PF51BGSQ>
2025-01-15 13:10:46 +0200

    Add project files.

---
+++ Harjoitus/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Harjoitus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kanta");
+            int kanta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Korkeus");
+            int korkeus = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Pinta-ala: {kanta * korkeus / 2}");
+        }
+    }
+}
---
+++ Testi1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Testi1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pizza on valmis");
+            Console.WriteLine("Kenelle pizza on?");
+            string nimi = Console.ReadLine();
+            Console.WriteLine($"Kirjataan muistiin. Pizza menee henkilölle: {nimi}");
+        }
+    }
+}
