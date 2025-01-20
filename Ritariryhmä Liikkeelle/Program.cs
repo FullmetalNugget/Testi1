242cebbe80fb62fa5bdfd64800c78ab8ed352b16
s2401221 <s2401221@BC-PF51BGSQ>
2025-01-20 13:44:06 +0200

    Dildo mestarei

--- Ritariryhmä Liikkeelle/Program.cs
+++ Ritariryhmä Liikkeelle/Program.cs
@@ -9,6 +9,7 @@
             int r = int.Parse(Console.ReadLine());
             Console.Write("Anna kuninkaan sarake? ");
             int c = int.Parse(Console.ReadLine());
+            Console.Beep();
             Console.ForegroundColor = ConsoleColor.DarkRed;
             if (r > 1) Console.WriteLine($"Ritari 1 Kohteeseen: ({r - 1}, {c})");
             if (r < 8) Console.WriteLine($"Ritari 2 Kohteeseen: ({r + 1}, {c})");
